572e33a912107d0ac841abded46672bb1c49341a
wen <asd82861892@gmail.com>
2022-05-22 22:37:39 +0800

    first commit

---
+++ mylinebot/foodlinebot/setting.py
@@ -0,0 +1,3 @@
+# ALLOWED_HOSTS = [
+#     'fee4-140-114-77-237.jp.ngrok.io'
+# ]
---
+++ mylinebot/foodlinebot/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# 這邊就是撰寫LINE Bot接收訊息後，所要執行的運算邏輯，
+# 這邊先以使用者發送什麼訊息，就回覆什麼訊息為例，來測試Django應用程式(APP)能夠成功的和LINE頻道(Channel)進行連結
+
+# from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+ 
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextSendMessage
+ 
+line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
+parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
+ 
+ 
+@csrf_exempt
+def callback(request):
+ 
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+ 
+        try:
+            events = parser.parse(body, signature)  # 傳入的事件
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+ 
+        for event in events:
+            if isinstance(event, MessageEvent):  # 如果有訊息事件
+                line_bot_api.reply_message(  # 回復傳入的訊息文字
+                    event.reply_token,
+                    TextSendMessage(text=event.message.text)
+                )
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
